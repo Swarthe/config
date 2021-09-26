dc1a93ce2b73ebf0fe8e96d200b696b0bef7ea34
Emil Overbeck <emil.a.overbeck@gmail.com>
2021-09-26 21:57:51 +0200

    improved media management

--- polybar/script/media.sh
+++ polybar/script/media.sh
@@ -5,8 +5,8 @@
 
 printf_media ()
 {
-    playerctl metadata -f "{{artist}}   $*   {{title}}" \
-    | sed -e 's/ - Topic//g' -e 's/\.[^.]*$//'
+    playerctl metadata -f \
+    "{{artist}}   $1   {{trunc(title, 75)}}   ({{duration(position)}} / {{duration(mpris:length)}})"
 }
 
 if [ "$(playerctl status 2> /dev/null)" = "Playing" ]; then
